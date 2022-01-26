14b99a408763bb26c8e5f7e1cfe6c5eb5f3d4860
David Hill <dhill@mindcry.org>
2022-01-25 21:33:35 -0500

    Use a buffered channel for signal notification

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	signals := make(chan os.Signal)
+	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
 	go func() {
 		for sig := range signals {
